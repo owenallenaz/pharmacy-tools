1aecc707a3ff8a7dbbb749253c12eda55f6e6447
root <root@DESKTOP-9LF5R27.localdomain>
2020-10-04 20:09:49 -0700

    Updates

---
+++ precache-manifest.3fb7d608999085f856a7a735dd95e38c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95b7120aea99adb592c668948c8a555",
+    "url": "/pharmacy-tools/index.html"
+  },
+  {
+    "revision": "1ed8feb396c5f312d108",
+    "url": "/pharmacy-tools/static/css/main.f1a6c620.chunk.css"
+  },
+  {
+    "revision": "e48f301c813ac620cd7a",
+    "url": "/pharmacy-tools/static/js/2.a357f000.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pharmacy-tools/static/js/2.a357f000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed8feb396c5f312d108",
+    "url": "/pharmacy-tools/static/js/main.2da020e7.chunk.js"
+  },
+  {
+    "revision": "e917b30e663858ab8f64",
+    "url": "/pharmacy-tools/static/js/runtime-main.6dc9168d.js"
+  }
+]);
